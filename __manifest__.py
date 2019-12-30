e51de0b275f158d04180fca1d0e876d96d3a2bbc
NgyenTrong <nqt@topica.edu.vn>
2019-12-30 10:25:49 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Manage Class',
+    'version' : '1.0',
+    'summary': '',
+    'sequence': 1,
+    'description': """
+
+    """,
+    'category': 'Accounting',
+    'website': 'https://www.odoo.com/page/billing',
+    'depends' : [
+        'base'
+    ],
+    'data': [
+
+    ],
+    'author': 'Nguyen Quang Trong',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
